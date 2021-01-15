dbc01fb0b1c76a2355909272406ecd8693d0fbeb
TheFrostSpartan <eswim009@gmail.com>
2021-01-15 15:14:10 -0700

    Added Pillar Switching

--- JakePaulJavaScript.js
+++ JakePaulJavaScript.js
@@ -7,3 +7,39 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   } 
+
+  function togglePillar1() {
+    var text = document.getElementById("pillar1");
+    var text2 = document.getElementById("pillar1Alt");
+    if (text.style.display == "none") {
+      text.style.display = "block";
+      text2.style.display = "none";
+    } else {
+      text.style.display = "none";
+      text2.style.display = "flex";
+    }
+  }
+
+  function togglePillar2() {
+    var text = document.getElementById("pillar2");
+    var text2 = document.getElementById("pillar2Alt");
+    if (text.style.display == "none") {
+      text.style.display = "block";
+      text2.style.display = "none";
+    } else {
+      text.style.display = "none";
+      text2.style.display = "flex";
+    }
+  }
+
+  function togglePillar3() {
+    var text = document.getElementById("pillar3");
+    var text2 = document.getElementById("pillar3Alt");
+    if (text.style.display == "none") {
+      text.style.display = "block";
+      text2.style.display = "none";
+    } else {
+      text.style.display = "none";
+      text2.style.display = "flex";
+    }
+  }
